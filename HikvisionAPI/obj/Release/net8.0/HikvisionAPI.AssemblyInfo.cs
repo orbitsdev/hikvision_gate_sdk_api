f3d67c38c3af585734bed2119fe070b16b5e62f1
Brian Orbino <orbinobrian0506@gmail.com>
2025-09-25 15:51:38 +0800

    update vuild

--- HikvisionAPI/obj/Release/net8.0/HikvisionAPI.AssemblyInfo.cs
+++ HikvisionAPI/obj/Release/net8.0/HikvisionAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HikvisionAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+582938b5668c94f35ba412a0c28a8c54d8ee0b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb775c4885e05b9fb6b6e415c5af70d72c43b0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HikvisionAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HikvisionAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
